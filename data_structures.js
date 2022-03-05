7633b4a10f950b57d623a7c3f330c9b2fbf98d6c
topoika <topoikatampul@gmail.com>
2022-03-06 00:31:00 +0300

    getSum problem

--- data_structures.js
+++ data_structures.js
@@ -1,6 +1,7 @@
-let data = [5, 3, 8, 6, 7, 2];
+let data = [5, 3, 8, 6, 9, 7, 2];
 
 const inserstionSort = (data) => {
+  // console.timeLog();
   for (let i = 1; i < data.length; i++) {
     let current = data[i];
     let j = i - 1;
@@ -10,8 +11,9 @@ const inserstionSort = (data) => {
     }
     data[j + 1] = current;
   }
+  console.log(data);
 };
-// inserstionSort(data);
+inserstionSort(data);
 // O(N^2) Quadratic
 
 const bubbleSort = (data) => {
@@ -28,6 +30,37 @@ const bubbleSort = (data) => {
       console.log(data);
     }
   }
+  binarySearch(data);
 };
 
-bubbleSort(data); //Time complextity = O(n^3)  Worst case scenario
+const binarySearch = (data) => {
+  let beggining = 0;
+  let n = data.length;
+  let searched = 6;
+  let middle = 0;
+  while (data[middle] != searched) {
+    middle = (n - beggining) / 2;
+    if (data[middle] != searched) {
+      beggining = middle;
+    } else {
+      console.log("go the the number at " + middle);
+    }
+  }
+};
+
+//Search for two numbers in an array that adds up to the target
+
+const getSums = (data, target) => {
+  for (let i = 0; i < data.length - 1; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      if (data[i] + data[j] == target) {
+        console.log([i, j]);
+      }
+    }
+  }
+};
+
+let numbers = [2, 4, 5, 4];
+getSums(numbers, 8);
+
+// bubbleSort(data); //Time complextity = O(n^3)  Worst case scenario
